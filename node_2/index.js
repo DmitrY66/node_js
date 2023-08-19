dd2524daddb6b911fbb1a03d516d24308d17eb80
Dmitri Gorun <ss808@mail.ru>
2023-08-19 19:12:34 +0500

    com_2

---
+++ node_2/index.js
@@ -0,0 +1,10 @@
+const { dataObj } = require('mpackage');
+
+
+const person = {
+  name: 'максим Лескин',
+  dateBirth: '10.03.1987',
+  purpose: 'карьерный рост'
+}
+
+dataObj(person);
---
+++ node_2/index.mjs
@@ -0,0 +1,9 @@
+import dataObj from 'mpackage';
+
+const person = {
+  name: 'максим лескин',
+  dateBirth: '10.03.1987',
+  purpose: 'карьерный рост'
+};
+
+dataObj(person);
